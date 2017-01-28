7576845e3731fa2d46d8a223b47ae044d1cbb974
vkochegin <vkochegin@gmail.com>
2017-01-28 21:44:19 +0200

    Adding new test cases

---
+++ src/test/java/com/telran/pages/TenderClientVitaliyPage.java
@@ -0,0 +1,66 @@
+package com.telran.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by vitaliy on 28.01.2017.
+ */
+public class TenderClientVitaliyPage extends Page{
+//FILES
+//==========================>> NAVIGATION BAR ON THE TOP<<==========================//a
+    @FindBy(xpath = "//*[contains(text(), 'Home')]")
+    private WebElement HomeLink;
+
+    @FindBy(xpath = "//*[@href='#!/masterView']")
+    private WebElement MasterViewLink;
+
+    @FindBy(xpath = "//*[@href='#!/mountingView']")
+    private WebElement MountingViewLink;
+
+    @FindBy(xpath = "//*[@href='#!/carWashView']")
+    private WebElement CarWashViewLink;
+
+    @FindBy(xpath = "//*[@href='#!/towTruckView']")
+    private WebElement TowTruckViewLink;
+
+    @FindBy(xpath = "//*[@href='#!/#!/commentsView']")
+    private WebElement CommentsViewLink;
+
+    @FindBy(xpath = "//*[@href='#!/tenderView']")
+    private WebElement TenderViewLink;
+
+    @FindBy (xpath = "//*[@class='navbar-form navbar-left ng-pristine ng-valid']//input")
+    private WebElement SearchField;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    private  WebElement SubmitButton;
+
+    @FindBy (xpath = "//a[contains(text(),'Account')]")
+    private WebElement AccountSettingLink;
+
+    @FindBy (xpath = "//a[contains(text(),'Log Out')]")
+    private WebElement LogOut;
+
+    @FindBy (id="loggedIn")
+    private WebElement LoggedInAs;
+
+//================================================================================//
+
+
+
+
+
+    public TenderClientVitaliyPage(WebDriver driver) {
+        super(driver);
+        this.PAGE_URL = "https://secure-citadel-93919.herokuapp.com"+"/#!/tenderView";
+        PageFactory.initElements(driver, this);
+    }
+
+public TenderClientVitaliyPage GoToTenderView (){
+driver.get(PAGE_URL);
+    return  this;
+}
+}
